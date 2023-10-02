63c34a4dcaadb72b5256a766b34fa72288fe04dc
root <root@UAB-5CG131748W.localdomain>
2023-10-02 13:56:28 +0200

    LOGIN: adds users to producers group if they're in producers table

--- plugins/Authentication/CAS/CAS.php
+++ plugins/Authentication/CAS/CAS.php
@@ -75,6 +75,39 @@ class CAS extends Authentication implements IAuthentication
         $attributes = phpCAS::getAttributes();
         $this->Synchronize($username, $attributes);
 
+        $session= parent::Login($username, $loginContext);
+        $userId = $session->UserId;
+        
+        // Productor group id
+        $addgroupid_productor_res = ServiceLocator::GetDatabase()->Query(new AdHocCommand("select group_id from groups where name = 'productors'"));
+                        
+        while ($row = $addgroupid_productor_res->GetRow())
+        {
+                $addgroupid_productor = $row['group_id'];
+        }
+
+        // Find out if user has to be productor
+        $isproductor_res = ServiceLocator::GetDatabase()->Query(new AdHocCommand("select count(*) from productors where username = \"$username\""));
+        $srow = $isproductor_res->GetRow();
+        $isproductor = $srow['count(*)'];
+
+        // Find out if user is in group productors
+        $ingroup_res = ServiceLocator::GetDatabase()->Query(new AdHocCommand("select count(*) from user_groups where user_id = \"$userId\" and group_id = \"$addgroupid_productor\""));
+        $srow = $ingroup_res->GetRow();
+        $ingroup = $srow['count(*)'];
+        
+        // If is not a productor and is in group, delete the group assignment
+        if(!$isproductor && $ingroup) {
+        
+                ServiceLocator::GetDatabase()->Execute(new AdHocCommand("delete from user_groups where user_id = $userId and group_id = $addgroupid_productor"));
+        }
+        // If is a productor and is not in group, insert the group assignment
+        if($isproductor && !$ingroup) {
+                ServiceLocator::GetDatabase()->Execute(new AdHocCommand("insert into user_groups (user_id, group_id) VALUES ($userId, $addgroupid_productor)"));
+        }
+
+
+
         return $this->authToDecorate->Login($username, $loginContext);
     }
 
